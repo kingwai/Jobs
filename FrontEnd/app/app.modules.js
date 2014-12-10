7542f1e08edfed8c1dd53db4a89982f4d56f3016
jzhang <jzzhang15@gmail.com>
2014-12-09 22:15:19 -0500

    Kick off project with angular route, bower and grunt

--- FrontEnd/app/app.modules.js
+++ FrontEnd/app/app.modules.js
@@ -1,3 +1,9 @@
 /**
  * Created by jzhang on 12/7/14.
  */
+//Define an angular module for our app
+
+angular.module("JoBillion", ['ngRoute']);
+
+
+
--- FrontEnd/app/app.routes.js
+++ FrontEnd/app/app.routes.js
@@ -2,4 +2,20 @@
  * Created by jzhang on 12/7/14.
  */
 
+//Define Routing for app
+angular.module('JoBillion').config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'app/logged_in.html',
+                controller: ''
+            }).
+            when('/home', {
+                templateUrl: 'app/talents/publish.html',
+                controller: ''
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
 
